012aab9533806651b0f6f895f4b0a748f3b06a63
Mugilan <mugilancodes@gmail.com>
2021-09-24 07:26:12 +0530

    initial commit :tada:

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Calendar</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
